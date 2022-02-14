bf054537d6b5ac16f0cf2e0d7b810f9cae95499c
jormig01 <99514557+jormig01@users.noreply.github.com>
2022-02-14 18:39:21 +0100

    Update 14th evening

--- LowTideStatistics.m
+++ LowTideStatistics.m
@@ -18,7 +18,7 @@ g = 9.81;                    % acceleration of gravity (m/s^2)
 rho = 1025;                  % water density (kg/m^3)
 fs = 2;                      % sampling frequency (Hz)
 Npos = 5;                    % number of cross-shore positions considered
-% More?
+% More? 
 
 % Initialisation vectors 
 % These vectors will be used to store the wave statistics at each position
@@ -31,11 +31,12 @@ Hm_tot   = zeros(Npos,1);  % mean wave height (m)
 % --------------------------------------
 
 for i=1:Npos  % loop on the positions
-    
-    % ?
-    % to be filled in
-    % ?
-    
+     %Frecuency is equal to 2 because the sampling ratio has aperiod of 0.5sec
+    wave = zero_crossing(data(:,i),2)
+    Hrms(i) = rms_height(data(:,i))
+    H13(i) = significant_height(data(:,i))
+    H(i) = mean(data(:,i)) %I DONT KNOW IF THIS IS CORRECT!!!!
+
 end
 
 % --------------------------------------
