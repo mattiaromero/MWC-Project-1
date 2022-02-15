ca8342ee49752db80314961fee2761d0d6382e17
jormig01 <99514557+jormig01@users.noreply.github.com>
2022-02-15 17:06:48 +0100

    it is raining

--- MWC_Project_1.m
+++ MWC_Project_1.m
@@ -38,7 +38,7 @@ h = ha+hb; %m
 hmean = mean(h);
 
 % Creating a time vector in seconds corresponding to the time-series
-t = (linspace(0,D*60,D*4*60))';
+t = (linspace(0,D*4*60-1,D*4*60))';
 
 % Plot of the evolution of h as a function of time. 
 figure; %CAREFUL FIX X AXIS to put in in Seconds
@@ -51,7 +51,6 @@ xlabel('Time [s]','FontWeight','bold');
 ylabel('Water depth [m]','FontWeight','bold');
 legend('Water depth','Mean water depth');
 grid on;
-xlim([0 3600]);
 savefig('Matlab1_i');
 
 % It can be seen in the figure that the water level is increasing with time. 
@@ -68,10 +67,9 @@ hold on;
 plot(t,h-hmean);
 title('Time evolution of the water depth h [m]');
 xlabel('Time [s]','FontWeight','bold');
-ylabel('Water level variation [m]','FontWeight','bold');
-legend('Tidally averaged water level variation','h-h_{mean}');
+ylabel('Water depth [m]','FontWeight','bold');
+legend('Tidally averaged water depth','h-h_{mean}');
 grid on;
-xlim([0 3600]);
 savefig('Matlab1_ii');
 
 %% Remark
@@ -82,70 +80,43 @@ savefig('Matlab1_ii');
 %Loading the files LowTide.txt and highTide.txt
 lowTide = load('lowTide.txt');
 highTide = load('highTide.txt');
-lowP1 = lowTide(:,1);
-lowP3 = lowTide(:,2);
-lowP6 = lowTide(:,5);
+lowP1 = lowTide(:,1)
+lowP3 = lowTide(:,2)
+lowP6 = lowTide(:,5)
 
-highP1 = highTide(:,1);
-highP3 = highTide(:,2);
-highP6 = highTide(:,5);
+highP1 = highTide(:,1)
+highP3 = highTide(:,2)
+highP6 = highTide(:,5)
+%Creating the new t vector
 
 % Sampling frequency
 dt = 1/2; 
 
 % Duration of the signal in minutes
-D = length(lowP1); 
-D = D/(2*60); %min
+D = length(lowP1); %34min
+D = D/(2*60); 
 
-%Creating the new time vector
-tt = (linspace(0,D*60,D*2*60))'; %new t vector for this section
+tt = (linspace(0,D*2*60-1,D*2*60))'; %new t vector for this section
+%CAREFUL FIX X AXIS to put in in Seconds
+subplot(3,2,1)
+plot(tt,lowP1)
+title("Low P1")
+subplot(3,2,2)
+plot(tt,highP1)
+title("High P1")
 
-%Plot high and low tide waves at P1,P3 and P6
-figure;
-subplot(3,2,1);
-plot(tt,lowP1);
-ylabel('Water level variation [m]','FontWeight','bold');
-title("Low tide at P1"); 
-grid on;
-xlim([0 D*60]);
-ylim([-1.5 1.75]);
+subplot(3,2,3)
+plot(tt,lowP3)
+title("Low P3")
+subplot(3,2,4)
+plot(tt,highP3)
+title("High P3")
 
-subplot(3,2,2);
-plot(tt,highP1);
-title("High tide at P1"); 
-grid on;
-xlim([0 D*60]);
-ylim([-1.5 1.75]);
+subplot(3,2,5)
+plot(tt,lowP6)
+title("Low P6")
+subplot(3,2,6)
+plot(tt,highP6)
+title("High P6")
 
-subplot(3,2,3);
-plot(tt,lowP3);
-ylabel('Water level variation [m]','FontWeight','bold');
-title("Low tide at P3"); 
-grid on;
-xlim([0 D*60]);
-ylim([-1.5 1.75]);
 
-subplot(3,2,4);
-plot(tt,highP3);
-title("High tide at P3"); 
-grid on;
-xlim([0 D*60]);
-ylim([-1.5 1.75]);
-
-subplot(3,2,5);
-plot(tt,lowP6);
-xlabel('Time [s]','FontWeight','bold');
-ylabel('Water level variation [m]','FontWeight','bold');
-title("Low tide at P6"); 
-grid on;
-xlim([0 D*60]);
-ylim([-1.5 1.75]);
-
-subplot(3,2,6);
-plot(tt,highP6);
-xlabel('Time [s]','FontWeight','bold');
-title("High tide at P6"); 
-grid on;
-xlim([0 D*60]);
-ylim([-1.5 1.5]);
-savefig('Matlab1_iii');
