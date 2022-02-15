6b2bc131ebe7fb997a36c6ad5997d1138f72111a
mattiaromero <mattiaromero10@gmail.com>
2022-02-15 16:58:53 +0100

    fix issue

--- rms_height.m
+++ rms_height.m
@@ -1,10 +1,8 @@
 function [Hrms]=rms_height(vectorh)
     n = length(vectorh);
-    h = sort(vectorh,"descend");
-    
+    h = sort(vectorh,'descend');
     Hrms = 0;
     for i = 1:n
         Hrms = Hrms + h(i).^2; 
     end
-
     Hrms = sqrt(1/n*Hrms);
--- significant_height.m
+++ significant_height.m
@@ -1,12 +1,10 @@
 function [H13]=significant_height(vectorh)
     n = length(vectorh);
-    h = sort(vectorh,"ascend");
-    H = 0;
+    h = sort(vectorh,'ascend');
+    H13 = 0;
     for i = round(2/3*n+1):n
-        H = H + h(i);
-                
-    end
-    
-    H13 = 1/(1/3*n)*H;
+        H13 = H13 + h(i);              
+    end  
+    H13 = 1/(1/3*n)*H13;
     
     
